f38b671629db06f17769770f944ca1a4a94ffe0c
Ai-Elaina <374709505@qq.com>
2021-04-07 09:44:26 +0800

    修改了autoload.js中的URL使其指向我的live2d-widget

--- autoload.js
+++ autoload.js
@@ -1,4 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
+const live2d_path_css = "https://cdn.jsdelivr.net/gh/Ai-Elaina/live2d-widget@latest/";
 const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
@@ -27,7 +28,7 @@ function loadExternalResource(url, type) {
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
-		loadExternalResource(live2d_path + "waifu.css", "css"),
+		loadExternalResource(live2d_path_css + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
